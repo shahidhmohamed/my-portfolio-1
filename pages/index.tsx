83a6aca13a21b63fdca3e426e3fe4d41a6878427
shahidhmohamed <mohamedshahidh81@gmail.com>
2024-04-05 09:17:13 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import About from "@/Components/About";
+import Hero from "@/Components/Hero";
+import MobileNav from "@/Components/MobileNav";
+import Nav from "@/Components/Nav";
+import React, { useState } from "react";
+
+const HomePage = () => {
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        {/* Navbar */}
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
+        {/* Hero section */}
+        <Hero />
+        <div className="relative z-[30]">
+          <About />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
